e35ea2f67c4fc0e9b5d899889874772a868e06b2
Andy Kilgore <92450298+kilgoreandy@users.noreply.github.com>
2023-02-20 00:29:43 -0600

    Update Projectile.cs
    @gamingdynamo Possible Project file. Notice the count of player projectiles should no longer stay alive no matter how many are spawned in . (projectiles)

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,14 +4,37 @@ public class Projectile : MonoBehaviour
 {
     public int damage;
     public bool friendly_projectile;
-    private void OnCollisionEnter(Collision collision)
+    public float destroy_delay = 1; // Time to wait before destroying the projectile
+
+    private float destroy_timer; // Timer to count down to destruction
+
+    private void Start()
     {
-        try 
+        // Start the timer when the projectile is instantiated
+        destroy_timer = destroy_delay;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<IDamageable>(out var damageable))
         {
-            collision.gameObject.GetComponent<IDamageable>().damage(damage, friendly_projectile);
-        } 
-        catch{ } // projectile didn't hit a damagable
-        
-        Destroy(gameObject);
+            // Damage the object if it is damageable
+            damageable.damage(damage, friendly_projectile);
+    	    // Reset the timer when the projectile collides with anything
+            destroy_timer = destroy_delay;
+        }
+
+    
+    }
+
+    private void Update()
+    {
+        // Update the timer and destroy the projectile when it reaches zero
+        destroy_timer -= Time.deltaTime;
+
+        if (destroy_timer <= 0f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
